625a54bf041ee97147bea3f4062a4972fb94ad0f
Mauricio-Alonso <conta.alonso@gmail.com>
2022-10-11 00:58:57 -0300

    Terminado el cartel de ciudad inexistente

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,16 +3,14 @@ import DisplayWeather from './DisplayWeather';
 import './weather.css'
 
  function Weather() {
-
-const APIKEY = "79b26ce8648741ed82a5a6d5cbd5af63";
-
+const [weather, setWeather] = useState([]);
 const [form, setForm] = useState({
     city:"",
     country:""
 
 });
 
-const [weather, setWeather] = useState([]);
+const APIKEY = "79b26ce8648741ed82a5a6d5cbd5af63";
 
 async function weatherData(e){
     e.preventDefault();
@@ -23,13 +21,8 @@ async function weatherData(e){
     .then((res) => res.json())
     .then (data => data);
 
-    setWeather(
-        {
-            data : data
-        }
-
-    );
-}
+    setWeather({data : data});
+    }
 }
 
 const handleChange = (e) => {
@@ -37,47 +30,31 @@ const handleChange = (e) => {
     let value = e.target.value;
 
     if (name === "city"){
-        setForm ({...form, city:value})
+        setForm ({...form, city:value});
 
     }
         if (name === "country"){
         setForm ({...form, country:value})
 
     }
-
-
 };
-
-
-
 
   return (
     <div className="weather">
         <span className="title">Weather App</span>
         <br/>
-
         <form>
             <input type="text" name="city" placeholder="city" onChange={e => handleChange(e)}></input>
             &nbsp;&nbsp;&nbsp;&nbsp;
             <input type="text" name="country" placeholder="country" onChange={e => handleChange(e)}></input>
             <button className="getweather" onClick={(e) => weatherData(e)}>Submit</button>
-
-
         </form>
 
-    {
-
-        weather.data !== undefined ?
-
+    {weather.data != undefined ? (
         <div>
             <DisplayWeather data={weather.data} />
         </div>
-
-
-        : null
-    }
-
-
+       ) : null}
     </div>
     
   )
